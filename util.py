3ea5e0697d658b79b978270a94a3a066d0eb9a4d
Weifeng Hu <weifhu@umich.edu>
2018-09-06 16:00:39 +0800

    iniital commit for utility functions

---
+++ util.py
@@ -0,0 +1,24 @@
+import prepare_data as pdata
+import numpy as np
+
+# load shuffled data and label
+def load_data_label():
+    # use prepare_data.py to load data
+    positve_processed = 'data/processed/positive/'
+    negative_processed = 'data/processed/negative/'
+    pos_data = pdata.read_data(positve_processed, label=1)
+    neg_data = pdata.read_data(negative_processed, label=0)
+    # concatenate and shuffle positive and negative samples
+    X_data_label = np.concatenate((pos_data, neg_data))
+    np.random.shuffle(X_data_label)
+    # separate data and label
+    data = []
+    label = []
+    for data_label in X_data_label:
+        data.append(data_label[0])
+        label.append(data_label[1])
+    return np.array(data), np.array(label)
+
+# test
+if __name__ == '__main__':
+    load_data_label()
